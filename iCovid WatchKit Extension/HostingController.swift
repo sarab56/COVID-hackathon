834353063a74b4cf1cda4bb47f11346766ac8c74
Emmanuel Salcedo <mail@emmanuelsalcedo.com>
2020-04-05 20:05:55 -0700

    Initial Commit

---
+++ iCovid WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  iCovid WatchKit Extension
+//
+//  Created by Emmanuel Salcedo on 4/5/20.
+//  Copyright © 2020 Emmanuel Salcedo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
